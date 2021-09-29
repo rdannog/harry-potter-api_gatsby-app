c672fa4a863b0bb98e2df006b431e15d6e74505c
rhayssadandara <rhayssadandara98@gmail.com>
2021-09-29 20:26:58 -0300

    adding homepage route

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,22 @@
 import React from "react"
 import Logo from "../../images/logo.png"
 import { BoxLogo, LogoImg } from "./style"
+import { Link } from "gatsby"
 
 export function Header(){
 
     return(
+        <>
         <BoxLogo>
             <LogoImg src={Logo} alt="logo Harry Potter"/>
         </BoxLogo>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home678hjbn </Link>
+                </li>
+            </ul>
+        </nav>
+        </>
     )
 }
